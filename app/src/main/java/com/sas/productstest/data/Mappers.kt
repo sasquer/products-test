f0a63a2233165d86ae3b8c153d87241f8ec33883
penzhulyak <penzhulyak@rollncode.com>
2026-06-13 12:59:24 +0300

    processed nullable fields

--- app/src/main/java/com/sas/productstest/data/Mappers.kt
+++ app/src/main/java/com/sas/productstest/data/Mappers.kt
@@ -13,44 +13,44 @@ private val gson = Gson()
 
 fun ProductDto.toEntity(): ProductEntity = ProductEntity(
     productId = productId,
-    name = name,
-    description = description,
-    price = price,
-    unit = unit,
-    image = image,
-    discount = discount,
-    availability = availability,
-    brand = brand,
-    category = category,
-    rating = rating,
-    reviews = gson.toJson(reviews.map { it.toReviewEntity() })
+    name = name ?: "",
+    description = description ?: "",
+    price = price ?: 0.0,
+    unit = unit ?: "Piece",
+    image = image ?: "",
+    discount = discount ?: 0,
+    availability = availability ?: false,
+    brand = brand ?: "",
+    category = category ?: "",
+    rating = rating ?: 0.0,
+    reviews = gson.toJson((reviews ?: emptyList()).map { it.toReviewEntity() })
 )
 
 fun ReviewDto.toReviewEntity(): ReviewEntity = ReviewEntity(
-    userId = userId,
-    rating = rating,
-    comment = comment
+    userId = userId ?: 0,
+    rating = rating ?: 0,
+    comment = comment ?: ""
 )
 
 fun ProductDto.toDomain(): Product = Product(
     productId = productId,
-    name = name,
-    description = description,
-    price = price,
-    unit = unit,
-    image = image,
-    discount = discount,
-    availability = availability,
-    brand = brand,
-    category = category,
-    rating = rating,
-    reviews = reviews.map { it.toDomain() }
+    name = name ?: "",
+    description = description ?: "",
+    price = price ?: 0.0,
+    unit = unit ?: "Piece",
+    image = image ?: "",
+    discount = discount ?: 0,
+    availability = availability ?: false,
+    brand = brand ?: "",
+    category = category ?: "",
+    rating = rating ?: 0.0,
+    reviews = (reviews ?: emptyList()).map { it.toDomain() }
 )
 
 fun ReviewDto.toDomain(): Review = Review(
-    userId = userId,
-    rating = rating,
-    comment = comment
+    userId = userId ?: 0,
+    rating = rating ?: 0,
+    comment = comment ?: ""
 )
 
 fun ProductEntity.toDomain(): Product {
